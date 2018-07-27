fdf77ed0433e3f9ef1c1b43dafd65d209b78826a
dyuki <unblivion@gmail.com>
2018-07-27 13:49:08 -0300

    ta funfando

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const comando = process.argv[2];
 const nome = process.argv[3];
 const email = process.argv[4];
+const fs = require("fs");
+const arquivo = "cadastros.json";
 
 let cadastros = {};
+
+fs.readFile(arquivo, (err, dados)=>{
+    let dadosString = dados.toString();
+    let cadastros = JSON.parse(dadosString);
+
+    if(comando === "salvar"){
+        cadastros[nome]= email;
+    
+        let dados = JSON.stringify(cadastros);
+    
+        fs.writeFile("cadastros.json", dados, (err)=>{
+            if(err){
+                console.log("Deu Ruim");
+            }
+            else{
+                console.log("Salvo Com sucesso");
+            }
+        });
+    }
+    else if(comando === "buscar"){
+        console.log(cadastros[nome]);
+    }
+    else if(comando === "buscar-todos"){
+        console.log(cadastros);
+    }
+    else{
+        console.log("Ta Na Disney ? ");
+    }
+    
+    cadastros[nome] = email;
+})
 
 /*
 console.log(nome);
 console.log(comando);
 console.log(email);
 */
-
-if(comando === "salvar"){
-    cadastros[nome]= email;
-}
-else if(comando === "buscar"){
-    console.log(cadastros[nome]);
-}
-else if(comando === "buscar-todos"){
-    console.log(cadastros);
-}
-else{
-    console.log("Ta Na Disney ? ");
-}
-
-cadastros[nome] = email;
-
-console.log(cadastros);
